62e2c35cab0dc3949892cb5ea29daa7535034303
sherinkd <sherinkd@live.com>
2023-09-26 00:52:29 -0400

    feat(ISM-672): add the best seller details

--- NWTradersWeb/Models/clsProduct.cs
+++ NWTradersWeb/Models/clsProduct.cs
@@ -8,6 +8,24 @@ using System.ComponentModel;
 namespace NWTradersWeb.Models
 {
     public partial class Product
-    {   
+    {
+        public List<string> GetSoldRegions() {
+            return Order_Details
+                .Select(od => od.Order.ShipRegion)
+                .Where(region => !string.IsNullOrEmpty(region) && !string.IsNullOrWhiteSpace(region))
+                .OrderBy(region => region)
+                .Distinct()
+                .ToList();
+        }
+
+        public List<string> GetSoldEmployees()
+        {
+            return Order_Details
+                .Select(od => od.Order.Employee)
+                .Select(employee => employee.LastName + ", " + employee.FirstName)
+                .OrderBy(employee => employee)
+                .Distinct()
+                .ToList();
+        }
     }
 }
