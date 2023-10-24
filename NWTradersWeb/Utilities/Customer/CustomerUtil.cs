ac73affcef69253c19654cfd73dbfb3ab4274031
sherinkd <sherinkd@live.com>
2023-10-24 07:46:32 -0400

    chore(product): ability to create a new product

---
+++ NWTradersWeb/Utilities/Customer/CustomerUtil.cs
@@ -0,0 +1,27 @@
+﻿using NWTradersWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWTradersWeb.Utilities
+{
+    public class CustomerUtil
+    {
+        private static NorthwindEntities nwEntities = new NorthwindEntities();
+
+       public Dictionary<string, int> CalculateTotalSalesRevenueByCustomer()
+        {
+            var customerSales = nwEntities.Orders
+                .GroupBy(order => order.Customer.ContactTitle + ", " + order.Customer.ContactTitle)
+                .Select(group => new
+                {
+                    CustomerName = group.Key,
+                    TotalSalesRevenue = 1
+                })
+                .ToDictionary(item => item.CustomerName, item => item.TotalSalesRevenue);
+
+            return (Dictionary<string, int>)customerSales.Take(1);
+        }
+    }
+}
