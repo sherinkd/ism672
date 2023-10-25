ffefec96bc0e6475bef927eff11c7895eb7a6f41
sherinkd <sherinkd@live.com>
2023-10-25 17:42:52 -0400

    chore(order): add employee login page

---
+++ NWTradersWeb/Models/clsEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWTradersWeb.Models
+{
+    public partial class Employee
+    {
+        public List<BestSellingProduct> BestSellingProducts(int limit)
+        {
+            return Orders
+                .SelectMany(s => s.Order_Details)
+                .Select(od => od.Product)
+                .GroupBy(s => s.ProductName, s => s.ProductID, (key, g) => new BestSellingProduct { ProductName = key, NumberOfSales = g.ToList().Count() })
+                .OrderByDescending(s => s.NumberOfSales)
+                .Take(limit)
+                .ToList();
+        }
+    }
+
+    public class BestSellingProduct
+    {
+        public string ProductName { get; set; }
+        public int NumberOfSales { get; set; }
+    }
+}
