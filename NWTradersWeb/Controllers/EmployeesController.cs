8e107c8b1dc776ab7ddd6bbd5616587248c9686e
sherinkd <sherinkd@live.com>
2023-11-28 13:05:12 -0500

    chore(visual): add all employees chart

--- NWTradersWeb/Controllers/EmployeesController.cs
+++ NWTradersWeb/Controllers/EmployeesController.cs
@@ -70,6 +70,146 @@ namespace NWTradersWeb.Controllers
             return View(employee);
         }
 
+        #region Utilities
+
+        public double CompanyAverageAnnualOrders()
+        {
+            double averageAnnualOrders = 0D;
+
+            averageAnnualOrders = nwEntities.
+                Orders.
+                Where(o => o.OrderDate.Value.Year <= 2020).
+                GroupBy(empOrder => new
+                {
+                    emp = empOrder.EmployeeID,
+                    Year = empOrder.OrderDate.Value.Year
+                }).
+                    Select(avgOrder => new
+                    {
+                        nOrders = avgOrder.Count()
+                    }).
+                    Average(o => o.nOrders);
+
+
+            return averageAnnualOrders;
+        }
+
+        public double EmployeeAverageAnnualOrders(int? EmployeeID)
+        {
+            double averageAnnualOrders = 0D;
+
+            if ((EmployeeID == null) || (nwEntities.Employees.Find(EmployeeID) == null))
+                return averageAnnualOrders;
+
+            averageAnnualOrders = nwEntities.
+                Orders.
+                Where(o => o.EmployeeID == EmployeeID).
+                Where(o => o.OrderDate.Value.Year <= 2020).
+                GroupBy(empOrder => new
+                {
+                    emp = empOrder.EmployeeID,
+                    Year = empOrder.OrderDate.Value.Year
+                }).
+                    Select(avgOrder => new
+                    {
+                        nOrders = avgOrder.Count()
+                    }).
+                    Average(o => o.nOrders);
+
+
+            return averageAnnualOrders;
+        }
+
+        public List<ProductSales> CompanyTop10Products()
+        {
+            return nwEntities.Order_Details.
+                    Where(od => (od.Product.Discontinued == false)).
+                    GroupBy(od => od.Product.ProductName).
+                    Select(prodSales => new ProductSales()
+                    {
+                        ProductName = prodSales.Key,
+                        Sales = prodSales.Sum(od => od.Quantity * od.UnitPrice)
+                    }).
+                    OrderByDescending(pSale => pSale.Sales).
+                    Take(10).
+                    ToList();
+        }
+
+        public List<CustomerSales> AllEmployeeTop10Customers()
+        {
+
+            return nwEntities.Order_Details.
+                    Where(od => (od.Product.Discontinued == false)).
+                    GroupBy(od => od.Order.Customer.CompanyName).
+                    Select(custSales => new CustomerSales()
+                    {
+                        theCustomer = custSales.Key,
+                        Sales = custSales.Sum(od => od.Quantity * od.UnitPrice)
+                    }).
+                    OrderByDescending(pSale => pSale.Sales).
+                    Take(10).
+                    ToList();
+        }
+
+        public decimal CompanyTotalRevenues()
+        {
+            return nwEntities.Order_Details.Sum(od => od.UnitPrice * od.Quantity);
+        }
+
+        public decimal EmployeeTotalRevenues(int? EmployeeID)
+        {
+            if (EmployeeID == null || nwEntities.Employees.Find(EmployeeID) == null)
+                return 0;
+
+            return nwEntities.Order_Details.
+                Where(od => od.Order.EmployeeID == EmployeeID).
+                Sum(od => od.UnitPrice * od.Quantity);
+        }
+
+        public double AllEmployeesAverageAnnualRevenues()
+        {
+            double averageAnnualRevenues = 0D;
+
+            averageAnnualRevenues = nwEntities.Order_Details.
+                GroupBy(od => new
+                {
+                    emp = od.Order.EmployeeID,
+                    year = od.Order.OrderDate.Value.Year
+                }).
+                Select(
+                ods =>
+                    ods.Sum(od =>
+                        ((double)od.UnitPrice - ((double)od.UnitPrice * (double)od.Discount)) * od.Quantity)).
+                    Average();
+
+            return averageAnnualRevenues;
+        }
+
+        public double EmployeeAverageAnnualRevenues(int? EmployeeID)
+        {
+            double averageAnnualRevenues = 0D;
+
+            if ((EmployeeID == null) || (nwEntities.Employees.Find(EmployeeID) == null))
+                return averageAnnualRevenues;
+
+
+            averageAnnualRevenues = nwEntities.Order_Details.
+                Where(od => od.Order.EmployeeID.Equals(EmployeeID)).
+                GroupBy(od => new
+                {
+                    year = od.Order.OrderDate.Value.Year
+                }).
+                Select(
+                ods =>
+                    ods.Sum(od =>
+                        ((double)od.UnitPrice - ((double)od.UnitPrice * (double)od.Discount)) * od.Quantity)).
+                    Average();
+
+            return averageAnnualRevenues;
+        }
+
+        #endregion
+
         #region AllEmployeeSales
 
         public ActionResult AllEmployeeSales(string Year = "All Years")
@@ -120,6 +260,112 @@ namespace NWTradersWeb.Controllers
             return Json(new { JSONList = allEmployeeSales }, JsonRequestBehavior.AllowGet);
         }
 
+        #endregion
+
+        #region Annual Orders
+
+        public ActionResult AnnualOrders()
+        {
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+            IEnumerable<OrderProducts> annualOrders = theEmployee.AnnualOrders();
+
+            ViewBag.CompanyAverageAnnualOrders = CompanyAverageAnnualOrders();
+            ViewBag.EmployeeAverageAnnualOrders = EmployeeAverageAnnualOrders(theEmployee.EmployeeID);
+
+            return PartialView("_AnnualOrders", annualOrders);
+        }
+
+        public JsonResult GetAnnualOrders()
+        {
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+            IEnumerable<OrderProducts> annualOrders = theEmployee.AnnualOrders();
+
+            return Json(new { JSONList = annualOrders }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AnnualOrdersModal(string Year = "")
+        {
+            int year = DateTime.Now.Year;
+
+            if (string.IsNullOrEmpty(Year))
+                year = int.Parse(Year);
+            else
+                year = int.Parse(Year);
+
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+            IEnumerable<OrderProducts> annualOrders = theEmployee.AnnualOrdersInYear(Year);
+
+            ViewBag.Year = year;
+            return PartialView("_AnnualOrdersModal", annualOrders);
+        }
+
+        public JsonResult GetAnnualOrdersModal(string Year = "")
+        {
+
+            int year = DateTime.Now.Year;
+
+            if (string.IsNullOrEmpty(Year))
+                year = int.Parse(Year);
+
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+            IEnumerable<OrderProducts> annualOrders = theEmployee.AnnualOrdersInYear(Year);
+
+            return Json(new { JSONList = annualOrders }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        #endregion
+
+        #region Annual Revenues
+
+        public ActionResult AnnualRevenues()
+        {
+            Employee currentEmployee = Session["currentEmployee"] as Employee;
+            currentEmployee = nwEntities.Employees.Find(currentEmployee.EmployeeID);
+            IEnumerable<OrderRevenues> annualRevenues = currentEmployee.AnnualRevenues();
+
+            ViewBag.CompanyAverageAnnualRevenues = AllEmployeesAverageAnnualRevenues();
+            //ViewBag.EmployeeAverageAnnualRevenues = EmployeeAverageAnnualRevenues(theEmployee.EmployeeID);
+            ViewBag.EmployeeAverageAnnualRevenues = currentEmployee.AverageAnnualRevenues();
+            return PartialView("_AnnualRevenues", annualRevenues);
+        }
+
+        public JsonResult GetAnnualRevenues()
+        {
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+            IEnumerable<OrderRevenues> annualRevenues = theEmployee.AnnualRevenues();
+
+            return Json(new { JSONList = annualRevenues }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllAnnualRevenues()
+        {
+
+            int year = DateTime.Now.Year;
+
+            Employee theEmployee = Session["currentEmployee"] as Employee;
+            theEmployee = nwEntities.Employees.Find(theEmployee.EmployeeID);
+
+            IEnumerable<OrderRevenues> allannualRevenues = nwEntities.Orders.
+                GroupBy(oo => oo.OrderDate.Value.Year).
+                Select(annual => new OrderRevenues
+                {
+                    Date = annual.Key.ToString(),
+                    Sales = annual.SelectMany(o => o.Order_Details).
+                                        Sum(oo => oo.UnitPrice * oo.Quantity)
+                }).
+                OrderBy(ans => ans.Date).
+                ToList();
+
+            return Json(new { JSONList = allannualRevenues }, JsonRequestBehavior.AllowGet);
+
+        }
         #endregion
 
         protected override void Dispose(bool disposing)
